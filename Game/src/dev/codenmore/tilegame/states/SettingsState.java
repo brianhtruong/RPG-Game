1096654a4e86e4578504b84f3fe9394b26549659
Brian Truong <brian-truong@berkeley.edu>
2017-01-02 23:33:36 -0800

    Added files

---
+++ Game/src/dev/codenmore/tilegame/states/SettingsState.java
@@ -0,0 +1,23 @@
+package dev.codenmore.tilegame.states;
+
+import java.awt.Graphics;
+
+import dev.codenmore.tilegame.Handler;
+
+public class SettingsState extends State {
+
+    public SettingsState(Handler handler) {
+	super(handler);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+
+}
